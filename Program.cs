e4de8783e357f5583a6d7dd97ad0cd15e4a5c524
Romario123 <kajdenkoroman35052@gmail.com>
2023-03-27 19:38:09 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+
+
+
+public class Program
+{
+    static long directorySize = 0;
+    static void Main(string[] args)
+    {
+        string dirName = Console.ReadLine();
+        string directory = dirName;
+        directorySize = sizeOfFolder(directory);
+        Console.WriteLine($" Size of directory {directory} - {directorySize}");
+
+
+
+
+
+
+
+    }
+
+    static long sizeOfFolder(string folder)
+    {
+        try
+        {
+
+            DirectoryInfo directory = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+
+                directorySize = directorySize + file.Length;
+            }
+
+            foreach (DirectoryInfo fdirectory in directories)
+            {
+
+                sizeOfFolder(fdirectory.FullName);
+            }
+
+            return directorySize;
+        }
+
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 0;
+        }
+
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 0;
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 0;
+        }
+    }
+}
+
